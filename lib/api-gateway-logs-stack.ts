705f3cb4952eebed6ea1b6a5d3d82e652bd9b631
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-12-09 12:46:24 +0100

    use RestApi construcy

--- lib/api-gateway-logs-stack.ts
+++ lib/api-gateway-logs-stack.ts
@@ -27,27 +27,34 @@ export class ApiGatewayLogsStack extends cdk.Stack {
       retention: RetentionDays.ONE_DAY,
     });
 
-    const apiLogs = new LogGroup(this, "api-access-logs", {
-      removalPolicy: RemovalPolicy.DESTROY,
-      retention: RetentionDays.ONE_DAY,
-    })
-    
-    const api = new ApiGateway.LambdaRestApi(this, "api", {
-      handler,
-      proxy: true,
+    // const apiLogs = new LogGroup(this, "api-access-logs", {
+    //   removalPolicy: RemovalPolicy.DESTROY,
+    //   retention: RetentionDays.ONE_DAY,
+    // })
+
+    const api = new ApiGateway.RestApi(this, "api", {
       deployOptions: {
         dataTraceEnabled: true,
-        accessLogDestination: new ApiGateway.LogGroupLogDestination(apiLogs),
-        accessLogFormat: ApiGateway.AccessLogFormat.jsonWithStandardFields(),
+        // accessLogDestination: new ApiGateway.LogGroupLogDestination(apiLogs),
+        // accessLogFormat: ApiGateway.AccessLogFormat.jsonWithStandardFields(),
         loggingLevel: ApiGateway.MethodLoggingLevel.INFO
       }
     });
 
-    new LogGroup(this, "api-log-group", {
-      logGroupName: `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`,
-      removalPolicy: RemovalPolicy.DESTROY,
-      retention: RetentionDays.ONE_DAY
-    });
+    api.root.addProxy({ defaultIntegration: new ApiGateway.LambdaIntegration(handler) })
+    const apiExecutionLogGroup = LogGroup.fromLogGroupName(
+      this,
+      "api-execution-log-group",
+      `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`
+    )
+
+    apiExecutionLogGroup.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    // new LogGroup(this, "api-log-group", {
+    //   logGroupName: `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`,
+    //   removalPolicy: RemovalPolicy.DESTROY,
+    //   retention: RetentionDays.ONE_DAY
+    // });
+
 
     new cdk.CfnOutput(this, 'API Gateway URL', {
       value: api.url as string,
