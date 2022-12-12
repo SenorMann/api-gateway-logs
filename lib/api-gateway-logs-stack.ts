67a592e3aee73b46161ec3981f54dd4a52e54c3c
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-12-12 17:40:41 +0100

    use default retention period for log group

--- lib/api-gateway-logs-stack.ts
+++ lib/api-gateway-logs-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnCustomResource, CfnElement, CfnResource, Duration, RemovalPolicy } from 'aws-cdk-lib';
+import { Duration, RemovalPolicy } from 'aws-cdk-lib';
 import * as ApiGateway from "aws-cdk-lib/aws-apigateway";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { CfnLogGroup, LogGroup, LogRetention, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
 import * as path from "path";
 
@@ -28,26 +28,32 @@ export class ApiGatewayLogsStack extends cdk.Stack {
       },
     });
 
-    api.applyRemovalPolicy(RemovalPolicy.DESTROY);
     api.root.addProxy();
 
+    const executionLogGroup = LogGroup.fromLogGroupName(
+      this,
+      "api-execution-log-group",
+      `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`
+    );
 
-    const lg = new LogRetention(this, "log-retention", {
-      logGroupName: `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`,
-      retention: RetentionDays.ONE_DAY,
-      removalPolicy: RemovalPolicy.DESTROY,
-      logRetentionRetryOptions: {},
-    });
+    executionLogGroup.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
-    lg.node.findAll().forEach((construct) => {
-      if (construct instanceof CfnResource) {
-        console.log("YES OH");
-        console.log(construct.logicalId)
-        if (construct.cfnResourceType === "AWS::Lambda::Function") {
-          // console.log(construct.)
-        }
-      }
-    })
+    // const lg = new LogRetention(this, "log-retention", {
+    //   logGroupName: `API-Gateway-Execution-Logs_${api.restApiId}/${api.deploymentStage.stageName}`,
+    //   retention: RetentionDays.ONE_DAY,
+    //   removalPolicy: RemovalPolicy.DESTROY,
+    //   logRetentionRetryOptions: {},
+    // });
+
+    // lg.node.findAll().forEach((construct) => {
+    //   if (construct instanceof CfnResource) {
+    //     console.log("YES OH");
+    //     console.log(construct.logicalId)
+    //     if (construct.cfnResourceType === "AWS::Lambda::Function") {
+    //       // console.log(construct.)
+    //     }
+    //   }
+    // })
 
     new LogGroup(this, "lambda-log-group", {
       logGroupName: `/aws/lambda/${handler.functionName}`,
