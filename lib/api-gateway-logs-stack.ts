57b1fc6d204c611534c486ecf44e15edba88a76b
Malik Mahmud <malikmahmud@safeguardworld.com>
2022-12-12 17:06:25 +0100

    [skip ci]: log more stuff

--- lib/api-gateway-logs-stack.ts
+++ lib/api-gateway-logs-stack.ts
@@ -42,7 +42,10 @@ export class ApiGatewayLogsStack extends cdk.Stack {
     lg.node.findAll().forEach((construct) => {
       if (construct instanceof CfnResource) {
         console.log("YES OH");
-        console.log(construct.cfnResourceType)
+        if (construct.cfnResourceType === "AWS::Lambda::Function") {
+          console.log(construct.logicalId)
+          // console.log(construct.)
+        }
       }
     })
 
